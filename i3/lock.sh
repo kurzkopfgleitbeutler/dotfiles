0a3446aadf4a9713db6e74b3377d00b16e5518fa
kurzkopfgleitbeutler <einfachzuverstehen@posteo.de>
2023-04-13 10:59:31 +0200

    fix lockscreen script

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
+
 img=/tmp/i3lock.png
+#img=/usr/share/wallpapers/Next/contents/images/1920x1080.png
 
-scrot -o $img
-convert $img -scale 10% -scale 1000% $img
+if [ $(command -v import) ] && [ $(command -v convert) ]
+then
+    #scrot -o $img
+    # BUG: ffmpeg -f x11grab -framerate 1 -video_size 1920x1080 -i :0.0 -vframes 1 "$img"
+    # from imagemagick:
+    import -window root "$img"
+    convert "$img" -scale 10% -scale 1000% "$img"
+else
+    img1=/usr/share/wallpapers/Next/contents/images/1920x1080.png
+    img2=/usr/share/wallpapers/Next/contents/images_dark/1920x1080.png
+    r=$(( $(dd if=/dev/urandom count=1 2> /dev/null | cksum | cut -d' ' -f1) % 2 ))
+    if [ "$r" -eq 1 ]
+    then
+	img=$img1
+    else
+	img=$img2
+    fi
+fi
 
-i3lock -u -i $img
+i3lock --no-unlock-indicator --image="$img" --pointer=win --ignore-empty-password
